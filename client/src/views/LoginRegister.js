2701e89e1757a91d5eaf57fae73d16cb74041a15
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-05-08 16:49:25 +0300

    Login Register Component Fix

--- client/src/views/LoginRegister.js
+++ client/src/views/LoginRegister.js
@@ -8,10 +8,7 @@ const LoginRegister = () => {
     <Container className="my-5">
       <Row className="justify-content-center">
         <Col xs={10} md={8}>
-          <Row className="mb-3">
-            <h2>Project Manager</h2>
-          </Row>
-          <Row>
+          <Row className="my-5">
             <Col xs={12} md={6} className="me-md-3">
               <RegisterForm />
             </Col>
